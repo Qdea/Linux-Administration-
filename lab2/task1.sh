6ab0fff11725abae2b57e066bdbdad4e45901a6c
Qdea <58660439+Qdea@users.noreply.github.com>
2021-10-28 21:23:30 +0300

    Update task1.sh

--- lab2/task1.sh
+++ lab2/task1.sh
@@ -1,3 +1,70 @@
-#!/bin/bash
-
-(echo n; echo p; echo ""; echo ""; echo "+300M"; echo w) | fdisk /dev/sda
+1)	fdisk /dev/sda
+	n
+	p
+	3
+	14551040 (first block)
+	+300M
+	w
+2)	sudo blkid /dev/sda3 > UUID_disk3
+3)	mkfs.ext4 -b 4096 /dev/sda3
+4)	dumpe2fs -h /dev/sda3
+5)	tune2fs -i 2m -C 2 /dev/sda3
+6)	cd /mnt
+	mkdir newdisk
+	mount /dev/sda3 /mnt/newdisk
+7)	ln -s /mnt/newdisk ~/newdisk
+8)	mkdir /mnt/newdisk/anyname
+9)	vi /etc/fstab
+	"/dev/sda3	/mnt/newdisk	ext4	noatime,nodiratime,noexec	0 0"
+	reboot
+10)	umount /dev/sda3
+	fdisk /dev/sda
+	d
+	3
+	n
+	p
+	3
+	14551040
+	+350M
+	w
+	resize2fs /dev/sda3
+	mount /dev/sda3
+	df -h
+11)	e2fsck -n /dev/sda3
+12)	fdisk /dev/sda
+	n
+	p
+	+12M
+	w
+	mkfs.ext4 /dev/sda4
+	sudo tune2fs -J location=/dev/sda4 /dev/sda3
+13)	fdisk /dev/sda
+	d
+	4
+	n
+	e
+	w
+	fdisk /dev/sda
+	n
+	+100M
+	w
+	fdisk /dev/sda
+	n
+	+100 M
+	w
+14)	reboot
+	mkdir /mnt/supernewdisk
+	yum install lvm2
+	pvcreate /dev/sda5 /dev/sda6
+	pvscan
+	vgcreate vol_grp1 /dev/sda5 /dev/sda6
+	lvcreate -L 20M -n logical_vol1 vol_grp1
+	mkfs.ext4 /dev/vol_grp1/logical_vol1
+	mount /dev/vol_grp1/logical_vol1 /mnt/supernewdisk
+15)	yum install samba
+	smbpasswd -a $USER
+	{rootroot}
+	yum install cifs-utils
+	mount -t cifs -o username=Everyone,password= //192.168.1.49/linux_share /mnt/share
+16) 	vi /etc/fstab
+	"//192.168.1.49/linux_share	/mnt/share	cifs	user=Everyone,password=,ro	0 0"
